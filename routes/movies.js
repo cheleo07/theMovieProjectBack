a5f60def56f17dde16ae68c83456cde64b878dd1
rodica <adigbononrodicaa@gmail.com>
2022-04-20 16:42:31 +0200

    ...

--- routes/movies.js
+++ routes/movies.js
@@ -1,4 +1,3 @@
-const dataFile = require('../local-data.js')
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const api = require('../api_key.js');
 
@@ -6,11 +5,9 @@ const api = require('../api_key.js');
 module.exports = (app) => {
 
     app.get('/api/discover/movie', async (req, res) => {
-        console.log('working');
-
         const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key='+ api +'&language=fr-FR',
             {       method: 'GET',
-                    headers: {'Content-Type': 'application/json'}
+                headers: {'Content-Type': 'application/json'}
             });
         const data = await response.json();
         return res.status(200).send(JSON.stringify(data));
